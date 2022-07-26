ce1e6efddd2f27f80571fe13e478fa198d1373b7
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-26 14:57:31 +0200

    Feat: Header nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,15 @@
 <template>
 <header>
-    <Navbar />
+    <div class="container-lg">
+    <div class="row justify-content-between pt-4">
+        <div class="col-1">
+        <img id="header-logo" src="../../public/Img/avadabarbers-logo-x1.png" alt="">
+        </div>
+        <Navbar
+        :NavbarItems = NavbarItems
+         />
+    </div>
+    </div>
     <Promos />
 </header>
 </template>
@@ -15,10 +24,41 @@ export default {
     components:{
         Navbar,
         Promos
-    }
+    },
+    data() {
+        return {
+            NavbarItems:[
+                //navbar due item o tre ? il logo è compreso?
+            /*     {
+                    item:0,
+                    link:'#',
+                    image:'../../public/Img/avadabarbers-logo-x1.png'
+                }, */
+                {
+                    item:1,
+                    link:"#",
+                    class:'bi bi-cart-fill'
+
+                },
+                {
+                    item:2,
+                    link:'#',
+                    class:'bi bi-list bi-2x'
+                }
+            ],
+
+        }
+    },
 }
 </script>
 
-<style>
-
+<style lang="scss">
+    header{
+        height: 100vh;
+        color: white;
+        background-image: url('../../public/Img/avadabarbers-homepage-hero-bg.jpg');
+        background-position: top;
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,29 @@
 <template>
-  
+  <div class="col-2">
+    <ul>
+        <a href="item.link" v-for="(item, index) in NavbarItems" :key="index">
+          <li :class="item.class">
+
+          </li>
+        </a>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
+    props:{
+        NavbarItems: Array
+    }
 
 }
 </script>
 
-<style>
-
+<style lang="scss">
+ul{
+  li{
+    padding: 0.5rem;
+    font-size: 1.5rem;
+  }
+}
 </style>
