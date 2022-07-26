2fb7947ad5762a967abb8fa59402deb96ccf992d
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-26 13:13:21 +0200

    Feat: components header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+<header>
+    <Navbar />
+    <Promos />
+</header>
+</template>
+
+<script>
+import Navbar from './Navbar.vue';
+import Promos from './Promos.vue';
+
+
+export default {
+    name:'Header',
+    components:{
+        Navbar,
+        Promos
+    }
+}
+</script>
+
+<style>
+
+</style>
