4acf345a3ebdaffc3ba390c3b9b5eb99d981a000
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-26 20:34:18 +0200

    Feat: Service section and component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <About />
+    <Services />
   </div>
 </template>
 
 <script>
 import About from "./About.vue";
+import Services from "./Services.vue";
+
 export default {
     components:{
         About,
+        Services
     }
     
 
---
+++ src/components/Services.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container-lg text-center">
+    <div class="content d-flex justify-content-center flex-column">
+        <h6>
+            Mens grooming
+        </h6>
+        <h1>
+            Services
+        </h1>
+    </div>
+    <div class="row">
+    <ServiceCard
+    
+    />
+    </div>
+    <Button
+    btnContent = 'Read about our services'
+    />
+  </div>
+</template>
+
+<script>
+import Button from "./Commons/Button.vue";
+import ServiceCard from "./Commons/ServiceCard.vue";
+export default {
+    components:{
+        Button,
+        ServiceCard,
+
+    },
+    data() {
+        return {
+            services:[
+                {
+                    service:'Trim & Cut',
+                    description: ''
+                }
+            ]
+        }
+    },
+
+}
+</script>
+
+<style lang="scss">
+@import '../assets/style/Colors.scss';
+@import '../assets/style/MixIn.scss';
+
+.content{
+    margin: 0 auto;
+    margin-top: 5rem;
+    h1{
+        color: $TextGrey;
+        font-size: 5rem;
+    }
+}
+    
+</style>
