c250ddcb4ef85b5433b443cc19bb38de16e880dd
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-26 20:46:11 +0200

    Feat: Created service card and working JSON props

---
+++ src/components/Commons/ServiceCard.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    props:{
+        services: Array,
+
+    },
+    mounted() {
+        console.log(this.services)
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="row">
     <ServiceCard
-    
+    :services = 'services'
     />
     </div>
     <Button
@@ -33,8 +33,20 @@ export default {
             services:[
                 {
                     service:'Trim & Cut',
-                    description: ''
-                }
+                    description: 'Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.',
+                    img:'avadabarbers-trimcut-icon-before.png'
+                },
+                {
+                    service:'Wash & Dry',
+                    description: 'Take a seat in our fine leather chairs,lean back and let us lather you a fresh head in a luxurious fashion',
+                    img:'avadabarbers-washndry-icon.png'
+                },
+                {
+                    service:'Beard Tidy',
+                    description: 'Tame the tangles and untidy facial hairs like a gentleman with our Beard Tidy services from Avada Barbers',
+                    img:'avadabarbers-beardtrim-icon.png'
+                },
+
             ]
         }
     },
