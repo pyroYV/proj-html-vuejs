9b52079bb528ccf7854136775f0dbe1611ef904e
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-26 16:50:47 +0200

    Feat:creation button component

--- src/components/Commons/Button.vue
+++ src/components/Commons/Button.vue
@@ -1,13 +1,27 @@
 <template>
-  
+
+  <a href="#" class="bottone">
+    {{btnContent}}
+  </a>
+
 </template>
 
 <script>
 export default {
-
+    props:{
+        btnContent: String
+    }
 }
 </script>
 
-<style>
-
+<style lang ="scss" scoped>
+.bottone{
+    text-align: center;
+    padding: 0.8rem 2.5rem;
+    border: 2px solid;
+    border-color: #ae8652;
+    color: #ae8652;
+    font-size: 1.5rem;
+    text-transform: uppercase;
+}
 </style>
