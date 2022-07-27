6fcb2d1c27b064f84f76c777a295fbda7b100e36
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-27 23:28:15 +0200

    Feat: Blogpost component and styling

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -2,27 +2,79 @@
   <div class="d-flex text-center flex-column" id="blog">
     <div class="content">
         <h6>
-            What people say
+           Tales from the barber shop
         </h6>
         <h1>
-            Reviews
+            Recent Blog Posts
         </h1>
     </div>
+    <div class="container-lg">
+      <div class="row">
+        <BlogPost
+          class="col-4"
+          v-for="(item, index) in blogposts" :key="index"
+          :item = 'item'
+        />
+      </div>
+    </div>
+    <Button
+    class="align-self-center mt-5"
+      btnContent = "Read more from our blog"
+    />
   </div>
 </template>
 
 <script>
-export default {
+import Button from "./Commons/Button.vue";
+import BlogPost from "./BlogPost.vue";
 
+export default {
+  components:{
+    Button,
+    BlogPost,
+  },
+    data() {
+      return {
+        blogposts:[
+            {
+            title:'Avada Barbers Now Open',
+            post:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio culpa optio molestiae ad maiores?',
+            img:'/img/avadabarbers-second-blog-700x441.jpg'
+
+            },
+            {
+            title:'Choosing The Right Barber',
+            post:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio culpa optio molestiae ad maiores?',
+            img:'/img/avadabarbers-choosing-blog-320x202.jpg'
+            
+
+            },
+            {
+            title:'Quick Service Guide',
+            post:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio culpa optio molestiae ad maiores?',
+            img:'/img/avadabarbers-ourservice-blog-320x202.jpg'        
+
+            },
+        ]
+    }
+  },
+  
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../assets/style/Colors.scss';
+@import '../assets/style/Mixin.scss';
 
 #blog{
     background-color: $TextWhite;
-    height: 90vh;
-    padding-top: 12vh;
+    height: 120vh;
+    padding-top: 20vh;
+    h1{
+      color: $TextGrey;
+    }
+    h6{
+      font-size: 1rem;
+    }
 }
 </style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,46 @@
+<template>
+
+  <div class="blog-post">
+    <img :src="item.img" alt="">
+    <h3>{{item.title}}</h3>
+    <p>
+        {{item.post}}
+    </p>
+  </div>
+
+</template>
+
+<script>
+export default {
+    props:{
+        item: Object,
+
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/Colors.scss';
+.blog-post{
+    
+    *{
+        margin-bottom: 1.5rem;
+        padding: 0 1rem;
+
+    }
+    img{
+        height: 15rem;
+    }
+    h3{
+        font-family: 'Abril Fatface', sans-serif;
+        font-size: 2.5rem;
+        color:$BrandColor;
+    }
+    p{
+        font-size: 1.2rem;
+        line-height: 2rem;
+        color: $TextGrey;
+    }
+
+}
+</style>
