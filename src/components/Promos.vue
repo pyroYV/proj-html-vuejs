6c1a6564b1fd21177563e9838690e04c7a5e0a1f
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-27 08:49:47 +0200

    fix: promos padding and font size

--- src/components/Promos.vue
+++ src/components/Promos.vue
@@ -1,12 +1,12 @@
 <template>
 <div id="promos">
-    <div class="promotion p-2 text-center rounded mb-2"> 
+    <div class="promotion p-2 px-3 text-center rounded mb-2"> 
     <div><i class="bi bi-terminal-fill"></i></div>
     <div>
         Demos
     </div>
   </div>
-  <div class="promotion p-2 text-center rounded"> 
+  <div class="promotion p-2 px-3 text-center rounded"> 
         <div class="sale-price">{{saleprice}}</div>
         <div>
             On Sale
@@ -37,12 +37,12 @@ export default {
         font-weight: bolder;
         .sale-price{
             color: $HighlightGreen;
-            font-size: 1.8rem;
+            font-size: 2.2rem;
             position: relative;
             &:before{
                 content: '$';
                 position:absolute;
-                left: -3px;
+                left: -5px;
                 top: 0;
                 font-size: 1.2rem;
 
