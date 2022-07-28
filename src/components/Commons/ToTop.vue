7f054f175a546ffd30b07b379756af3b35b390b4
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-28 14:10:04 +0200

    Feat: Totop component working

---
+++ src/components/Commons/ToTop.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="arrow-up" class="position-absolute rounded" @click="scrollToTop()">
+    <i class="bi bi-arrow-bar-up"></i>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        scrollToTop() {
+            window.scrollTo(0, 0);
+        },
+    },
+}
+</script>
+
+<style>
+#arrow-up{
+    bottom: 0;
+    right: 3%;
+    padding: .4rem 1rem;
+    font-size: 1.5rem;
+    background-color: #303030;
+    color: white;
+    cursor: pointer;
+}
+</style>
